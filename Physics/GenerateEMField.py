463995363b45e0d44d62ef6292d4c0b72e842d2f
btryan1 <98588452+btryan1@users.noreply.github.com>
2023-11-03 13:49:23 +0000

    Create GenerateEMField.py

---
+++ Physics/GenerateEMField.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+from cmath import pi
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from scipy import signal
+
+class GeneralEMField(ABC):
+
+    @abstractmethod
+    def __init__(self, ElectricField = np.array([0,0,0], dtype=float), MagneticField = np.array([0,0,0], dtype=float)):
+        self.electric = np.array(ElectricField,dtype=float)
+        self.magnetic = np.array(MagneticField, dtype=float)
+        super(GeneralEMField, self).__init__()
+
+
+    def __repr__(self):
+        return 'EMField: E = {0}, B = {1}'.format(self.electric,self.magnetic)
+
+
+    def getAcceleration(self,particle):
+        lorentz = np.array(self.electric, dtype=float)
+        lorentz+=np.cross(particle.velocity, self.magnetic)
+        lorentz *= (particle.charge/particle.mass)
+        return lorentz
+    
+class EMField(GeneralEMField):
+        
+    def __init__(self,electric = np.array([0,0,0], dtype=float),magnetic = np.array([0,0,0], dtype=float)):
+        super().__init__(electric,magnetic)
+  
+    def TimePeriod(self,particle):
+        return (2*pi*particle.mass)/(particle.charge*np.linalg.norm(self.magnetic))
+
+    def Cyclotron_Frequency(self,particle):
+        return (particle.charge*np.linalg.norm(self.magnetic))/(particle.mass)
+
+    def Square_Wave_Gen(self,f_cyclo):
+        t = np.linspace(0, 0.2, 200000, endpoint=False)
+        square=signal.square(f_cyclo * t)
+        I=[]
+        for i in range(664):
+            I.append(1)
+        for j in range(len(square)):
+            I.append(square[j])
+        return I
